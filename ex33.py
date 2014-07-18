1ab5f36ec6510ba35af8ec5b1f1dda0a6e97ffac
grockcharger <dickerzhy1111@163.com>
2014-07-18 05:17:54 -0700

    add new file

---
+++ ex33.py
@@ -0,0 +1,18 @@
+# By guoshu
+# 2014/7/17 20:11
+
+i = 0
+numbers = []
+
+while i < 6:
+	print "At the top i is %d" % i
+	numbers.append(i)
+	
+	i += 1
+	print "Numbers now:", numbers
+	print "At the bottom i is %d" % i 
+
+print "The numbers:"
+
+for num in numbers:
+	print num
